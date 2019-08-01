b30489a003a52e5cfa14456ea40bc431f786f475
umutkurtr <umutkurtr@gmail.com>
2019-08-01 14:33:22 +0200

    .

---
+++ MVCSite/Controllers/PanelController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSite.Controllers
+{
+    public class PanelController : Controller
+    {
+        // GET: Panel
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
