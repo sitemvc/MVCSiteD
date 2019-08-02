d34a8a6505ace85a13876d22793b551554811100
umutkurtr <umutkurtr@gmail.com>
2019-08-02 10:26:36 +0200

    .

--- MVCSite/Controllers/PanelController.cs
+++ MVCSite/Controllers/PanelController.cs
@@ -4,16 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MVCSite.Controllers
 {
+    [Authorize]
     public class PanelController : Controller
     {
         // GET: Panel
-        [Authorize]
+
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Login");
         }
 
     }
